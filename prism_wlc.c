4a894fba12c97044520a47cb71d065906ca99340
GspiritTk <68810510+GspiritTk@users.noreply.github.com>
2024-11-04 14:56:17 +0000

    Add files via upload

--- prism_wlc.c
+++ prism_wlc.c
@@ -13,6 +13,43 @@ static double cq_pel(double q, double kuhn_length, double radius, double sigma, 
     return (sin(qRc) / qRc) * (exp(-pow(q, 2.0) * pow(sigma, 2.0)));
 }
 
+#define N 10000
+double Si(double x) {
+    if (x == 0.0) {
+        return 0.0;
+    }
+
+    double a = 0.0;
+    double b = x;
+    double h = (b - a) / N;
+    double sum = 0.0;
+    for (int i = 0; i <= N; i++) {
+        double t = a + i * h;
+        double coef;
+
+        if (i == 0 || i == N) {
+            coef = 1;
+        } else if (i % 2 == 0) {
+            coef = 2;
+        } else {
+            coef = 4;
+        }
+
+        double f_t = (t == 0) ? 1 : sin(t) / t;
+        sum += coef * f_t;
+    }
+
+    sum = (h / 3.0) * sum;
+    return sum;
+}
+
+static double cq_rod(double q, double lr)
+{
+    double term1 = 2 * Si(q * lr) / (q * lr);
+    double term2 = 4 * pow(sin(q * lr / 2), 2) / (pow(q, 2) * pow(lr, 2));
+    return term1 - term2;
+
+}
 
 static double
 Iq(double q,
@@ -21,8 +58,7 @@ Iq(double q,
    double radius,
    double sld,
    double solvent_sld,
-   double sigma,
-   double rc,
+   double lr,
    double parav,
    double conc,
    double wamw)
@@ -32,6 +68,6 @@ Iq(double q,
     const double pcs = pow(sas_2J1x_x(q*radius), 2);
     const double volume = M_PI*radius*radius*length;
     const double pwlc = Sk_WR(q, length, kuhn_length);
-    const double cq = cq_pel(q, kuhn_length, radius, sigma, rc);
+    const double cq = cq_rod(q, lr);
     return 1e8 * nump * pow(contrast, 2) * pwlc * pcs / (1 + parav * cq * pwlc);
 }
